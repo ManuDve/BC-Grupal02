0cb2f1efa9241cb81939aa34eb4955188b3091e0
ManuDv <m.alfaro@live.cl>
2023-01-30 18:35:42 -0300

    Agrega estilos y validación al formulario

--- js/main.js
+++ js/main.js
@@ -14,3 +14,39 @@ fecha.textContent = fechaActual;
 
 
 }, 1000)
+
+function calificar(){
+    let num = parseInt(document.getElementById("nota").value);
+
+    let text;
+    switch (true){
+        case num>= 0 && num<3 :
+            text = "Muy deficiente";
+            break;
+        case num>= 3 && num<5:
+            text = "Insuficiente";
+            break; 
+        case num>= 5 && num<6:
+            text = "Suficiente" ;
+            break; 
+        case num>= 6 && num<7:
+            text = "Bien";
+            break; 
+        case num>= 7 && num<9:
+            text = "Notable";
+            break; 
+        case num>= 9 && num<=10:
+            text = "Sobresaliente";
+            break;   
+        default: 
+            text = "Ingrese un número válido";
+    }
+    
+    if (num >= 0 && num <= 10) { // Condicional para el texto
+        document.getElementById("result").innerText= "Nuestro servicio está: " + text;
+        document.getElementById("numresult").innerText = "Tu calificación fue de: " + num;
+    } else { // Elimina el resultado anterior si se ingresa un número fuera del rango o algo que no sea un número
+        document.getElementById("numresult").innerText = "";
+        document.getElementById("result").innerText= "Ingrese un número válido";
+    }
+}
